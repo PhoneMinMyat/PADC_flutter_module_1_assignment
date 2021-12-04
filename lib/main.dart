aa764599ec4276d8a208f5f37855a1dd5473e45c
PhoneMinMyat <phoneminmyat1999@gmail.com>
2021-12-04 13:03:29 +0630

    fix some bugs

--- lib/main.dart
+++ lib/main.dart
@@ -51,136 +51,142 @@ class HomePage extends StatelessWidget {
           ),
         ],
       ),
-      body: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 15),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            const Text(
-              'Create account',
-              style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 35,
-                  color: Colors.white),
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            const TextField(
-              decoration: InputDecoration(
-                fillColor: Colors.white,
-                filled: true,
-                hintText: 'Name',
-                hintStyle: TextStyle(
-                  color: Color.fromRGBO(66, 81, 101, 1),
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            const TextField(
-              decoration: InputDecoration(
-                fillColor: Colors.white,
-                filled: true,
-                hintText: 'Your email address',
-                hintStyle: TextStyle(
-                  color: Color.fromRGBO(66, 81, 101, 1),
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            const TextField(
-              decoration: InputDecoration(
-                fillColor: Colors.white,
-                filled: true,
-                hintText: 'Create a password',
-                hintStyle: TextStyle(
-                  color: Color.fromRGBO(66, 81, 101, 1),
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 8,
-            ),
-            Row(
-              children: const [
-                Icon(
-                  Icons.info,
-                  color: Color.fromRGBO(0, 168, 225, 1),
-                ),
-                Text(
-                  'Passwords must be at least 6 characters',
-                  style: TextStyle(color: Colors.white, fontSize: 15),
-                )
-              ],
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                  primary: const Color.fromRGBO(13, 121, 175, 1),
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 15, horizontal: 30)),
-              onPressed: () => print('Create'),
-              child: const Text(
-                'Create your Amazon account',
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
-              ),
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            RichText(
-              text: TextSpan(style: const TextStyle(fontSize: 14), children: [
-                const TextSpan(
-                    text: 'By creating an account, you agree to the '),
-                TextSpan(
-                  text: 'Prime Video Terms of Use',
-                  recognizer: TapGestureRecognizer()
-                    ..onTap = () => print('Click Terms'),
-                  style: const TextStyle(
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const Text(
+                'Create account',
+                style: TextStyle(
                     fontWeight: FontWeight.bold,
-                    color: Color.fromRGBO(13, 121, 175, 1),
+                    fontSize: 35,
+                    color: Colors.white),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              const TextField(
+                decoration: InputDecoration(
+                  fillColor: Colors.white,
+                  filled: true,
+                  hintText: 'Name',
+                  hintStyle: TextStyle(
+                    color: Color.fromRGBO(66, 81, 101, 1),
                   ),
                 ),
-                const TextSpan(
-                    text:
-                        ' and license agreements which can be found on the Amazon website.')
-              ]),
-            ),
-            TextButton(
-              child: const Text(
-                'Already have an account?',
-                style: TextStyle(color: Colors.white, fontSize: 15),
               ),
-              onPressed: () => print('Already have an account?'),
-            ),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                  primary: const Color.fromRGBO(66, 81, 101, 1),
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 15, horizontal: 30)),
-              onPressed: () => print('Sign In'),
-              child: const Text(
-                'Sign-In now',
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
+              const SizedBox(
+                height: 15,
               ),
-            ),
-            const SizedBox(
-              height: 70,
-            ),
-            const Text(
-              '©️ 1996-2021, Amazon.com, Inc. or its affiliates',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                  color: Color.fromRGBO(103, 122, 133, 1), fontSize: 14),
-            )
-          ],
+              const TextField(
+                decoration: InputDecoration(
+                  fillColor: Colors.white,
+                  filled: true,
+                  hintText: 'Your email address',
+                  hintStyle: TextStyle(
+                    color: Color.fromRGBO(66, 81, 101, 1),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              const TextField(
+                decoration: InputDecoration(
+                  fillColor: Colors.white,
+                  filled: true,
+                  hintText: 'Create a password',
+                  hintStyle: TextStyle(
+                    color: Color.fromRGBO(66, 81, 101, 1),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              Row(
+                children: const [
+                  Icon(
+                    Icons.info,
+                    color: Color.fromRGBO(0, 168, 225, 1),
+                  ),
+                  Text(
+                    'Passwords must be at least 6 characters',
+                    style: TextStyle(color: Colors.white, fontSize: 15),
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    primary: const Color.fromRGBO(13, 121, 175, 1),
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 15, horizontal: 30)),
+                onPressed: () => print('Create'),
+                child: const Text(
+                  'Create your Amazon account',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
+                ),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              RichText(
+                text: TextSpan(style: const TextStyle(fontSize: 14), children: [
+                  const TextSpan(
+                      text: 'By creating an account, you agree to the '),
+                  TextSpan(
+                    text: 'Prime Video Terms of Use',
+                    recognizer: TapGestureRecognizer()
+                      ..onTap = () => print('Click Terms'),
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromRGBO(13, 121, 175, 1),
+                    ),
+                  ),
+                  const TextSpan(
+                      text:
+                          ' and license agreements which can be found on the Amazon website.')
+                ]),
+              ),
+              const Padding(
+                padding:  EdgeInsets.all(20.0),
+                child:  Center(
+                  
+                  child:  Text(
+                      'Already have an account?',
+                      style: TextStyle(color: Colors.white, fontSize: 15),
+                    ),
+                ),
+              ),
+               
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    primary: const Color.fromRGBO(66, 81, 101, 1),
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 15, horizontal: 30)),
+                onPressed: () => print('Sign In'),
+                child: const Text(
+                  'Sign-In now',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
+                ),
+              ),
+              const SizedBox(
+                height: 70,
+              ),
+              const Text(
+                '©️ 1996-2021, Amazon.com, Inc. or its affiliates',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: Color.fromRGBO(103, 122, 133, 1), fontSize: 14),
+              )
+            ],
+          ),
         ),
       ),
     );
